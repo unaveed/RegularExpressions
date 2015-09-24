e449eb3bc2414856e8b32552543704f6dce590b6
unaveed <unaveed@gmail.com>
2015-09-23 20:25:44 -0600

    Passing tests left and right

--- src/cs_3100_java/Part5.java
+++ src/cs_3100_java/Part5.java
@@ -4,19 +4,18 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Part4 {
+public class Part5 {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		Pattern pattern = Pattern.compile("\"(\\\\.)*.*?(\\\\.)*(\\\\\\\\|(?<!\\\\))\"");
+		Pattern pattern = Pattern.compile("<([^\"<>]|(\"<\")|(\"([^\\\"\\n]|(\\\"))*\"))*?[^<>\"]*>");
 		String input = "";
 		while(s.hasNextLine()){
 			input += s.nextLine() + "\n";
 		}
-		s.close();
 		Matcher m = pattern.matcher(input);
-		while(m.find()){
-			System.out.println(m.group());
-		}
+		System.out.println(m.replaceAll(""));
+		s.close();
 	}
+
 }
